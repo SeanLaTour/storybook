0e9bd11fff4d7ae323365e8be1c4f83a9918ff22
Sean La Tourette <sean.latourette@straightfire.tech>
2023-10-19 21:11:10 -0700

    first real commit

---
+++ src/pages/book.js
@@ -0,0 +1,44 @@
+import { indexOf } from "lodash"
+import * as React from "react"
+import backgroundImage from "../images/fotor-ai-20231019185725.jpg"
+import "../styles/defaultStyles.css"
+
+const currentPage = {
+    pageBackground: backgroundImage,
+    pageText: ["Are you like me?", "Do you wonder what's out there?", "If you're alone..."]
+}
+
+const BookPage = () => {
+    
+    return (
+        <div style={{height: "100vh", width: "100vw", backgroundColor: "black", padding: "1rem"}}>
+        <img style={{position: "fixed", top: "0px", left: "0px", width: "100vw", height: "100vh"}} src={backgroundImage}></img>
+        <div id="page-content-box" className="page-content" style={{position: "fixed", top: "0px", left: "0px", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <h3 id="page-content-text" style={{backgroundColor: "#222", borderRadius: "10px", padding: "1rem", opacity: ".7", color: "white"}}>Are you like me?</h3>
+        </div>
+        <button style={{position: "fixed", padding: ".25rem", opacity: .7, borderRadius: "2px", backgroundColor: "#222", color: "white", bottom: "0px", right: "0px"}} onClick={turnPage} >Next Page</button>
+        <button style={{position: "fixed", padding: ".25rem", opacity: .7, borderRadius: "2px", backgroundColor: "#222", color: "white", bottom: "0px", left: "0px"}} >Prev Page</button>
+        </div>
+    )
+}
+
+function turnPage() {
+    const pageElement = document.getElementById("page-content-box");
+    const textElement = document.getElementById("page-content-text");
+    const textIndex = currentPage.pageText.indexOf(textElement.innerHTML)
+    console.log(textElement.innerHTML, textIndex)
+    if(currentPage.pageText[textIndex + 1]) {
+        pageElement.classList.remove("page-content");
+        textElement.innerHTML = currentPage.pageText[textIndex + 1];
+        setTimeout(() => {
+            pageElement.classList.add("page-content");
+
+        }, 1);
+    }
+}
+
+
+
+export default BookPage
+
+export const Head = () => <title>Home Page</title>
