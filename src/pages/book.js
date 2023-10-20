ecd324484e5d3492298abc3bd5e02ac8e65a052f
Sean La Tourette <sean.latourette@straightfire.tech>
2023-10-19 21:35:42 -0700

    added image animation

--- src/pages/book.js
+++ src/pages/book.js
@@ -12,7 +12,7 @@ const BookPage = () => {
     
     return (
         <div style={{height: "100vh", width: "100vw", backgroundColor: "black", padding: "1rem"}}>
-        <img style={{position: "fixed", top: "0px", left: "0px", width: "100vw", height: "100vh"}} src={backgroundImage}></img>
+        <img id="background-image" style={{position: "fixed", top: "0px", left: "0px", width: "100vw", height: "100vh"}} src={backgroundImage}></img>
         <div id="page-content-box" className="page-content" style={{position: "fixed", top: "0px", left: "0px", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
             <h3 id="page-content-text" style={{backgroundColor: "#222", borderRadius: "10px", padding: "1rem", opacity: ".7", color: "white"}}>Are you like me?</h3>
         </div>
@@ -22,17 +22,27 @@ const BookPage = () => {
     )
 }
 
+function idleImageAnimation() {
+    const imageElement = document.getElementById("background-image");
+    setTimeout(() => {
+        imageElement.style.width = "125vw";
+        imageElement.style.height = "125vh";
+        imageElement.style.left = "-12.5vw";
+        imageElement.style.top = "-10vh";
+    }, 500);
+}
+
 function turnPage() {
+    idleImageAnimation();
     const pageElement = document.getElementById("page-content-box");
     const textElement = document.getElementById("page-content-text");
     const textIndex = currentPage.pageText.indexOf(textElement.innerHTML)
     console.log(textElement.innerHTML, textIndex)
     if(currentPage.pageText[textIndex + 1]) {
         pageElement.classList.remove("page-content");
-        textElement.innerHTML = currentPage.pageText[textIndex + 1];
         setTimeout(() => {
+            textElement.innerHTML = currentPage.pageText[textIndex + 1];
             pageElement.classList.add("page-content");
-
         }, 1);
     }
 }
