10c04575ac990d05785906357aa605cd8b906950
Sean La Tourette <sean.latourette@straightfire.tech>
2023-10-23 22:32:21 -0700

    the empty hide start

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import "../styles/defaultStyles.css"
 
 const IndexPage = () => {
   return (
-    <div style={{backgroundImage: "url(https://u-static.fotor.com/images/text-to-image/result/PRO-a6da605dc2a945e38dd11245d19d6654.jpg)", backgroundSize: "cover", height: "100vh", width: "100vw", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-      <h1 style={{textAlign: "center", paddingInline: "20vw"}}>There Once, We Were Many</h1>
+    <div style={{backgroundImage: "url(https://u-static.fotor.com/images/text-to-image/result/PRO-53a560aa2f8b42e1a95d41c043ac1540.jpg)", backgroundSize: "cover", height: "100vh", width: "100vw", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <h1 style={{textAlign: "center", paddingInline: "20vw", color: "white"}}>The Empty Hide</h1>
       <button onClick={() => {
         window.location.href = window.location.href + "book/";
       }} >Open Book</button>
